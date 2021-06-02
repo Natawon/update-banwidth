be715701556b56871a7e48176e54bc69894663a7
Natawon <natawon9@gmail.com>
2021-06-02 16:18:36 +0700

    add new .sh version

--- update-bandwidth.sh
+++ update-bandwidth.sh
@@ -4,10 +4,33 @@
 NIC=$1
 
 vn_value=$(vnstat -ru 1 -tr 5 -i $NIC) || exit 3 # We collect bandwidth for 2 second
-rx_value=$(echo "$vn_value"|grep rx|awk {'print $2'}|cut -d. -f1)
+rx_value=$(echo "$vn_value"|grep rx|awk {'print $2'} | bc)
 rx_unit=$(echo "$vn_value"|grep rx|awk {'print $3'}|cut -d. -f1)
-tx_value=$(echo "$vn_value"|grep tx|awk {'print $2'}|cut -d. -f1)
+tx_value=$(echo "$vn_value"|grep tx|awk {'print $2'} | bc)
 tx_unit=$(echo "$vn_value"|grep tx|awk {'print $3'}|cut -d. -f1)
+
+#echo "$vn_value"|grep rx|awk {'print $3'}|cut -d. -f1
+#echo "$vn_value"|grep rx|awk {'print $2'}
+#echo "$vn_value"|grep tx|awk {'print $2'}| bc
+#echo "$vn_value"|grep tx|awk {'print $3'}|cut -d. -f1
+
+x=1024
+y=1000000
+
+rx_total=$(echo $rx_value $x | awk '{printf $1*$2}')
+#echo $z;
+
+rx_totals=$(echo $rx_value $y | awk '{printf $1*$2}')
+#echo $zz;
+
+tx_total=$(echo $tx_value $x | awk '{printf $1*$2}')
+#echo $p;
+
+tx_totals=$(echo $tx_value $y | awk '{printf $1*$2}')
+#echo $pp;
+
+
+
 
 # Exit if its using byte
 echo $vn_value|grep 'iB'&&echo "##### Please configure /etc/vnstat.conf to display in bit #####"&&exit
@@ -15,16 +38,21 @@ echo $vn_value|grep 'iB'&&echo "##### Please configure /etc/vnstat.conf to displ
 #recalculate rx_value and tx_value, depending on the unit in rx_unit and tx_unit
 #first for rx
 if [ $rx_unit == "Mbit/s" ]
-    then rx_value_recal=$((rx_value*1024))
-    else rx_value_recal=$((rx_value))
+    then  rx_value_recal=$((rx_total))
+elif [ $rx_unit == "Gbit/s" ]
+    then  rx_value_recal=$((rx_totals))
+ else rx_value_recal=$((rx_value))
 fi
 
 
 #...then also for tx
-if [ $tx_unit == "Mbit/s" ]
-    then tx_value_recal=$((tx_value*1024))
-    else tx_value_recal=$((tx_value))
+if [ $tx_unit == "Mbit/s"  ]
+    then tx_value_recal=$((tx_total))
+elif [ $tx_unit == "Gbit/s" ]
+    then tx_value_recal=$((tx_totals))
+else tx_value_recal=$((tx_value))
 fi
+
 
 status="$rx_value_recal $tx_value_recal"
 
@@ -32,8 +60,10 @@ status="$rx_value_recal $tx_value_recal"
 #echo "NIC $NIC - rx: $rx_value_recal Kbps - tx: $tx_value_recal Kbps"
 #echo '{"server_name":"th-live-17.open-cdn.com","bandwidth_rx":"'$rx_value_recal'","bandwidth_tx":"'$tx_value_recal'"}'
 
-curl -v -H "Content-Type: application/json" --request POST -d '{"server_name":"43.229.149.12","bandwidth_rx":"'$rx_value_recal'","bandwidth_tx":"'$tx_value_recal'"}' http://test-api.deknerd.com/api/v1.0/thailivestream/bandwidth
-#curl -k -X POST -H "Content-Type: application/json" -d '{"server_name":"43.229.149.12","bandwidth_rx":"'$rx_value_recal'","bandwidth_tx":"'$tx_value_recal'"}' https://socket.deknerd.com:8091/api/v1.0/thailivestream/bandwidth
+#curl -k -X POST -H "Content-Type: application/json" -d '{"server_name":"setlive-stream-cdn.open-cdn.com","bandwidth_rx":"'$rx_value_recal'","bandwidth_tx":"'$tx_value_recal'"}' http://elearning.set.or.th:8001/api/site/bandwidths/current
+
+curl -k -X POST -H "Content-Type: application/json" -d '{"server_name":"setlive-stream2-cdn.open-cdn.com","bandwidth_rx":"'$rx_value_recal'","bandwidth_tx":"'$tx_value_recal'"}' https://lms.elearning.set.or.th/api/site/bandwidths/current
+
 
 # {
 #    "id":"4",
